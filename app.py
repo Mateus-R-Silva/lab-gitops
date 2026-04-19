c3e24dfb756e0a401a208af307a956c3daaca0be
Mateus Ribeiro <mateusribeirosilva123@gmail.com>
2026-04-19 17:45:16 -0300

    feat: teste ci

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def home()
     # Pega o nome do Pod no Kubernetes. Se não existir, usa "Local"
     nome_do_pod = os.getenv('HOSTNAME', 'Local')
     return f"<h1>App do Mateus v1.0</h1><p>Rodando no Pod: {nome_do_pod}</p>"
