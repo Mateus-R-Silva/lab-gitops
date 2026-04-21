d1ef8f357940a398528954e510fd845984ee954c
Mateus Ribeiro <mateusribeirosilva123@gmail.com>
2026-04-21 13:05:02 -0300

    feat: estruturando projeto com Kustomize (base e overlays)

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask
+from redis import Redis
 import os
 
 app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def home():
+    hits = redis.incr('hits')
     # Pega o nome do Pod no Kubernetes. Se não existir, usa "Local"
     nome_do_pod = os.getenv('HOSTNAME', 'Local')
-    return f"<h1>App do Mateus v1.0</h1><p>Rodando no Pod: {nome_do_pod}</p>"
-
+    return f"""
+    <h1>Sistema de Contagem de Visitas</h1>
+    <p>Esta página foi vista {hits} vezes.</p>
+    <p>Respondido no Pod: {nome_do_pod}</p>"
+    """
+    
 if __name__ == '__main__':
     # O container vai ouvir na porta 8080
     app.run(host='0.0.0.0', port=8080)
