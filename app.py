258d0c8753bb23e7e739c8ae62d1a49d5bf6609d
Mateus Ribeiro <mateusribeirosilva123@gmail.com>
2026-04-29 21:29:43 -0300

    fix: ajuste para comunicação com redis

--- app.py
+++ app.py
@@ -2,22 +2,31 @@ from flask import Flask
 from redis import Redis
 import os
 
-
 app = Flask(__name__)
-redis = Redis(host='redis-s', port=6379, decode_responses=True)
+
+# Configuração do Redis vinda do ambiente
+redis_host = os.getenv('REDIS_HOST', 'redis')
+cache = Redis(host=redis_host, port=6379, socket_connect_timeout=2)
 
 @app.route('/')
 def home():
     try:
-        hits = redis.incr('hits')
+        # Incrementa o contador no Redis
+        acessos = cache.incr('hits')
     except Exception as e:
-        hits = 'Erro de conexão com Redis'
-    # Pega o nome do Pod no Kubernetes. Se não existir, usa "Local"
+        acessos = f"Erro de conexão com Redis ({e})"
+
+    # Pega o nome do Pod no Kubernetes
     nome_do_pod = os.getenv('HOSTNAME', 'Local')
-    return f"""<h1>App Full Stack</h1>
-    <p>Quantidade de acessos: {hits}</p>
-    Rodando no Pod: {nome_do_pod}</p>"""
+
+    return f"""
+    <h1>App Full Stack</h1>
+    <p><b>Quantidade de acessos:</b> {acessos}</p>
+    <p><b>Rodando no Pod:</b> {nome_do_pod}</p>
+    <hr>
+    <p>Conectado ao Host: {redis_host}</p>
+    """
 
 if __name__ == '__main__':
-    # O container vai ouvir na porta 8080
+    # Flask rodando na 8080 para casar com o targetPort do seu Service
     app.run(host='0.0.0.0', port=8080)
